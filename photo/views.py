b8fe420326f27c97c2ef207f49546d73fe783ea9
seo-dong-hyeon <62382430+seo-dong-hyeon@users.noreply.github.com>
2021-03-03 14:40:07 +0900

    add photo view, url

--- photo/views.py
+++ photo/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView, CreateView, DeleteView
+from django.views.generic.list import ListView
 
-# Create your views here.
+from .models import Photo
+
+
+class PhotoList(ListView):
+    model = Photo
+    template_name_suffix = '_list'
+
+
+class PhotoCreate(CreateView):
+    model = Photo
+    fields = ['author','text', 'image']
+    template_name_suffix = '_create'
+    success_url = '/'
+
+
+class PhotoUpdate(UpdateView):
+    model = Photo
+    fields = ['author','text', 'image']
+    template_name_suffix = '_update'
+    # success_url = '/'
+
+
+class PhotoDelete(DeleteView):
+    model = Photo
+    template_name_suffix = '_delete'
+    success_url = '/'
+
+
+class PhotoDetail(DetailView):
+    model = Photo
+    template_name_suffix = '_detail'
+
