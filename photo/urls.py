7595ae2db5064957bb0552d40a2128f224e4dbe4
seo-dong-hyeon <62382430+seo-dong-hyeon@users.noreply.github.com>
2021-03-04 05:23:18 +0900

    add myList view

--- photo/urls.py
+++ photo/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.conf.urls import url, include
 from .views import PhotoList, PhotoDelete, PhotoDetail, PhotoUpdate, PhotoCreate, \
-    PhotoLike, PhotoFavorite, PhotoLikeList, PhotoFavoriteList
+    PhotoLike, PhotoFavorite, PhotoLikeList, PhotoFavoriteList, PhotoMyList
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -15,14 +15,15 @@ router = routers.DefaultRouter()
 router.register('photos', photo.api.PhotoViewSet)
 
 urlpatterns = [
+    path("", PhotoList.as_view(), name='index'),
     path("create/", PhotoCreate.as_view(), name='create'),
     path("delete/<int:pk>/", PhotoDelete.as_view(), name='delete'),
     path("update/<int:pk>/", PhotoUpdate.as_view(), name='update'),
     path("detail/<int:pk>/", PhotoDetail.as_view(), name='detail'),
-    path("", PhotoList.as_view(), name='index'),
     path("like/<int:photo_id>/", PhotoLike.as_view(), name='like'),
     path("favorite/<int:photo_id>/", PhotoFavorite.as_view(), name='favorite'),
     path("like/", PhotoLikeList.as_view(), name="like_list"),
+    path("mylist/", PhotoMyList.as_view(), name='mylist'),
     path("favorite/", PhotoFavoriteList.as_view(), name="favorite_list"),
     url(r'^api/v1/', include((router.urls, 'photo'), namespace='api')),
     url(r'^api/doc', get_swagger_view(title='Rest API Document')),
